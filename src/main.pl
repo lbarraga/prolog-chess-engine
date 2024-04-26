581259a6989554c3efa6f01eb03fa83c82ae78d4
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-04-26 21:20:22 +0200

    extra spatie fixen

--- src/main.pl
+++ src/main.pl
@@ -24,14 +24,20 @@ san_to_move_string(no_move, "") :- !.
 san_to_move_string(San, MoveString) :- phrase(San, List), atom_chars(MoveString, List).
 
 % Define a predicate to pretty print a list
-pretty_print(_, []).
-pretty_print(N, [turn(WhiteSan, BlackSan)|T]) :-
+pretty_print(Moves) :- pretty_print_(1, no_move, Moves).
+
+pretty_print_(_, no_move, []) :- !.
+pretty_print_(N, _, []) :- write(N), write(". ").
+pretty_print_(N, _, [turn(WhiteSan, BlackSan)|T]) :-
     write(N), write(". "),
     san_to_move_string(WhiteSan, WhiteMoveString),
     san_to_move_string(BlackSan, BlackMoveString),
-    write(WhiteMoveString), write(" "), write(BlackMoveString), write(" "),
+    write(WhiteMoveString), write(" "),
+    % Dont write anything if blacks an is no_move
+    (BlackSan = no_move -> true ; write(BlackMoveString), write(" ")),
+
     N1 is N + 1,
-    pretty_print(N1, T).
+    pretty_print_(N1, BlackSan, T).
 
 
 main :-
@@ -41,7 +47,7 @@ main :-
 
     % Parse the file into a list of moves.
     parse_file(FileName, Parsed),
-    pretty_print(1, Parsed),
+    pretty_print(Parsed),
 
     % Parse the moves into a board
     init_state(InitialState),
