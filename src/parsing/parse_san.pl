c664a3d05e6c56b573cad41d906e852b13455646
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-04-30 14:11:28 +0200

    Docs: parse_san.pl

--- src/parsing/parse_san.pl
+++ src/parsing/parse_san.pl
@@ -1,7 +1,7 @@
 :- module(parse_san, [san/4]).
 
-promoting_piece(P) --> bishop(P); knight(P); rook(P); queen(P).    % A piece which can be promoted to.
-piece(P) --> promoting_piece(P); king(P).                    % A general piece on the board.
+promoting_piece(P) --> bishop(P); knight(P); rook(P); queen(P).     % A piece which can be promoted to.
+piece(P) --> promoting_piece(P); king(P).                           % A general piece on the board.
 
 
 % Define individual characters for each chess piece.
@@ -11,7 +11,8 @@ knight(knight) --> "N".
 queen(queen)  --> "Q".
 rook(rook)   --> "R".
 
-% Define a chess coordinate
+% Define ranks and files. These are converted to 0-indexed integers, representing the row and column of the board.
+% (0, 0) is the top-left corner of the board.
 file(Col) --> {member(F, ["a", "b", "c", "d", "e", "f", "g", "h"]), Col is F - 97}, F.
 rank(Row) --> {member(R, ["1", "2", "3", "4", "5", "6", "7", "8"]), Row is 56 - R}, R.
 
@@ -25,22 +26,24 @@ checkmate --> "#".
 short_castle --> "O-O".
 long_castle --> "O-O-O".
 
+% Disambiguation can be a file, rank, or a coordinate.
 disambiguation(file(DisAmb)) --> file(DisAmb).
 disambiguation(rank(DisAmb)) --> rank(DisAmb).
 disambiguation(DisAmb) --> coordinate(DisAmb).
-
 
 % Unified rule for the destination part of a move, which includes the coordinate and an optional promotion
 destination(Co, Prom) --> coordinate(Co), optional_promotion(Prom).
 
 % Generalized move rules
-move(plie(P, Takes, Co, D, Prom)) --> piece(P), optional_disambiguation(D), optional_takes(Takes), destination(Co, Prom). % General piece move.
-move(plie(pawn, takes, Co, file(F), Prom)) --> file(F), takes, destination(Co, Prom).                              % Pawn capture.
-move(plie(pawn, no_takes, Co, none, Prom)) --> destination(Co, Prom).                                                 % Pawn move.
-move(castle(short)) --> short_castle.                                                                % Special move.
-move(castle(long)) --> long_castle.                                                                  % Special move.
+move(plie(P, Takes, Co, D, Prom)) -->
+    piece(P), optional_disambiguation(D), optional_takes(Takes), destination(Co, Prom). % General piece move.
+move(plie(pawn, takes, Co, file(F), Prom)) --> file(F), takes, destination(Co, Prom).   % Pawn capture.
+move(plie(pawn, no_takes, Co, none, Prom)) --> destination(Co, Prom).                   % Pawn move.
+move(castle(short)) --> short_castle.                                                   % short castle.
+move(castle(long)) --> long_castle.                                                     % long castle.
 
-% Define components of moves
+% == Define components of moves ==
+
 optional_disambiguation(DisAmb) --> disambiguation(DisAmb).
 optional_disambiguation(none) --> [].
 
@@ -55,5 +58,5 @@ optional_check_or_checkmate(checkmate) --> checkmate.
 optional_check_or_checkmate(check) --> check.
 optional_check_or_checkmate(no_check) --> [].
 
-% Top-level rule for moves including post-move conditions
+% Top-level rule for moves including post-move conditions (Check, Checkmate, or nothing)
 san(Move, Check) --> move(Move), optional_check_or_checkmate(Check).
