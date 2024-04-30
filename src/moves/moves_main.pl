94b22324a40116b3982b8a5c1cda4458d85313bf
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-04-30 13:43:01 +0200

    Docs: moves_main.pl

--- src/moves/moves_main.pl
+++ src/moves/moves_main.pl
@@ -4,78 +4,103 @@
 :- use_module(piece_rules).
 :- use_module(castling).
 
-legal_move(castle(Type), State, NewState) :-
-    castle(Type, State, NewState).
+% legal_move(?Move, +State, ?NewState)
+% Move is a legal move in the given State, resulting in NewState.
+% Move could be:
+%   - move(From, To)
+%   - castle(Type)
+%   - promotion(From, To, PromotingPieceName)
+%
+% This is the main predicate for checking if a move is legal.
+legal_move(castle(Type), State, NewState) :- castle(Type, State, NewState).
 
+% Promotion move
 legal_move(promotion(From, To, PromotingPieceName), state(Board, Info, Color), state(NewBoard, NewInfo, Opponent)) :-
     legal_move_promotion(move(From, To), state(Board, Info, Color), state(TempBoard, NewInfo, Opponent)),
-    % Check if the piece moved is a pawn
     get(From, Board, Piece),
-    name(Piece, pawn),
-    color(Piece, Color),
-    last_rank(Color, To),
-    name(PromotingPiece, PromotingPieceName),
-    is_promotable(PromotingPieceName),
-    color(PromotingPiece, Color),
-    place(To, PromotingPiece, TempBoard, NewBoard).
+    name(Piece, pawn),                              % The piece is a pawn.
+    color(Piece, Color),                            % The piece is of the correct color.
+    last_rank(Color, To),                           % The pawn is moving to the last rank.
+    name(PromotingPiece, PromotingPieceName),       % The promoting piece is a valid piece.
+    is_promotable(PromotingPieceName),              % The promoting piece is promotable (rook, knight, bishop, queen).
+    color(PromotingPiece, Color),                   % Get the correct atom for the promoting piece.
+    place(To, PromotingPiece, TempBoard, NewBoard). % Place the promoting piece on the board.
 
-
+% Normal move
 legal_move(move(From, To), State, NewState) :-
-    basic_move_unsafe(move(From, To), State, NewState),
+    basic_move_unsafe(move(From, To), State, NewState), % Check if move could be done, disregarding check.
     NewState = state(NewBoard, Info, Opponent),
-    opponent(Opponent, Color),
-    \+ king_in_check(state(NewBoard, Info, Color)),
-    % Check if it is not a pawn moving to the last last_rank
+    opponent(Opponent, Color),                          % Get the opponent color.
+    \+ king_in_check(state(NewBoard, Info, Color)),     % Check if the king is not in check after the move.
+
+    % Check if it is not a pawn moving to the last last_rank (if it is, it should be a promotion move.
     State = state(Board, _, _),
     get(From, Board, Piece),
     \+ (name(Piece, pawn), last_rank(Color, To)).
 
+% legal_move_promotion(+Move, +State, -NewState)
+% Same as legal_move, but only for promotion moves (no check is done for pawn on last rank).
 legal_move_promotion(move(From, To), State, NewState) :-
     basic_move_unsafe(move(From, To), State, NewState),
     NewState = state(NewBoard, Info, Opponent),
     opponent(Opponent, Color),
     \+ king_in_check(state(NewBoard, Info, Color)).
 
+% is_promotable(+PieceName)
+% Check if a this piece can be promoted to.
 is_promotable(queen).
 is_promotable(rook).
 is_promotable(bishop).
 is_promotable(knight).
 
+% last_rank(+Color, +Position)
+% Check if the position is the last rank for the given color.
 last_rank(white, (0, _)).
 last_rank(black, (7, _)).
 
-
+% basic_move_unsafe(+Move, +State, -NewState)
+% Checks if a move follows the piece movement rules, disregarding check.
 basic_move_unsafe(Move, State, NewState) :-
-    opposing_color_or_empty(Move, State),
-    get_name(Move, State, Name),
-    basic_piece_move_unsafe(Name, Move, State, NewState).
+    opposing_color_or_empty(Move, State),                   % Destination square is empty or contains an opponent piece.
+    get_name(Move, State, Name),                            % Get the name of the piece.
+    basic_piece_move_unsafe(Name, Move, State, NewState).   % Check if the piece can move to the destination square. (piece rule)
 
+% get_name(+Move, +State, -Name)
+% Get the name of the piece that is moving.
 get_name(move(From, _), state(Board, _, _), Name) :-
     get(From, Board, Piece),
     name(Piece, Name).
 
+% opposing_color_or_empty(+Move, +State)
+% Check if the destination square is empty or contains an opponent piece.
 opposing_color_or_empty(move(From, To), state(Board, _, Color)) :-
     get(From, Board, FromPiece),
     color(FromPiece, Color),
     get(To, Board, ToSquare),
-    could_capture(FromPiece, ToSquare).
+    could_capture(FromPiece, ToSquare). % The destination square is empty or contains an opponent piece.
 
-
+% king_in_check(+State)
+% Check if the king is in check in the given state.
 king_in_check(state(Board, _, Color)) :-
     king(Color, King),
     get(KingCo, Board, King),
     opponent(Color, OpponentColor),
-    basic_move_unsafe(move(_, KingCo), state(Board, _, OpponentColor), _).
+    basic_move_unsafe(move(_, KingCo), state(Board, _, OpponentColor), _). % Check if the king can be captured, disregarding pins or checks.
 
+% is_stalemate(+State)
+% Check if the game is in stalemate.
 is_stalemate(State) :-
     \+ king_in_check(State),    % King is not in check
     \+ legal_move(_, State, _). % No legal moves
 
+% is_checkmate(+State)
+% Check if the game is in checkmate.
 is_checkmate(State) :-
     king_in_check(State),       % King is in check
     \+ legal_move(_, State, _), !. % No legal moves
 
-% The new is_checkmate predicate for King of the Hill rules
+% is_checkmate(+State)
+% Additional check for checkmate in king of the hill. The opponent is checkmated when the king is on a central square.
 is_checkmate(State) :-
     rules(koth_rules),
     state(Board, _, Color) = State,
@@ -84,7 +109,8 @@ is_checkmate(State) :-
     get(OpponentKingCo, Board, OpponentKing),
     central_square(OpponentKingCo).     % King is on a central square
 
-% Define the central squares
+% central_square(+Position)
+% Check if the position is a central square.
 central_square((3,3)).
 central_square((3,4)).
 central_square((4,3)).
