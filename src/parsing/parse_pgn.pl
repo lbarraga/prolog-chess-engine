402e134eab0facd33b016df0bfc7afcad666a9c1
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-04-30 14:07:36 +0200

    Docs: parse_pgn.pl

--- src/parsing/parse_pgn.pl
+++ src/parsing/parse_pgn.pl
@@ -4,17 +4,20 @@
 :- use_module(library(pio)).
 :- use_module(library(dcg/basics)).
 
+% Parse a string between two other strings.
 between(Left, Between, Right) --> Left, string_without(Right, Between), Right.
 
 % Define the grammar for parsing PGN files.
 pgn(Moves, R) --> tags(R), movetext(Moves), result, blanks.
 
+% Parse the tags. If koth is present in the current parsed tag or in the other tags, the rules are koth.
 tags(koth_rules) --> tag(koth_rules), tags(_).
 tags(koth_rules) --> tag(_), tags(koth_rules).
 tags(normal_rules) --> tag(normal_rules), tags(normal_rules).
 tags(normal_rules) --> [].
 tags(normal_rules) --> eol.
 
+% Parse a single tag. We only care about a possible koth tag.
 tag(koth_rules) --> "[Rules \"koth\"]", eol.
 tag(normal_rules) --> between("[", _, "]"), eol.
 
@@ -24,6 +27,7 @@ plie(san(San, Check)) --> blanks, san(San, Check), blanks.
 move(San1, San2) --> move_nr, plie(San1), plie(San2).
 move_plie(San) --> move_nr, plie(San).
 
+% Parse the movetext as a list of plie tuples.
 movetext([turn(San1, San2) | Rest]) --> move(San1, San2), movetext(Rest).
 movetext([turn(San, no_move)]) --> move_plie(San).
 movetext([]) --> [].
